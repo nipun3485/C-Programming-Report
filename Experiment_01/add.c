3f8dd7d137c863f7f47b3986e19b8b10941dcfe4
nipun3485 <nipunxsingh@gmail.com>
2025-11-19 16:49:02 +0000

    Experiment 01 with all screenshots

---
+++ Experiment_01/add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+int a;
+int b;
+int sum;
+scanf("%d",&a);
+scanf("%d",&b);
+sum=a+b;
+printf("the sum of a and b is%d\n",sum);
+return 0;
+}
---
+++ Experiment_01/newline.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("My Address is:\n");
+    printf("Survey Chowk\n");
+    printf("18 E.C. Road\n");
+    printf("Hyde Colony\n");
+    printf("House No. 17/3\n");
+    return 0;
+}
